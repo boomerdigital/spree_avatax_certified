403cebe65d8ba3a0a6550b356564f03e25ab2c04
Jason King <herqueles3@gmail.com>
2014-04-30 12:49:33 -0400

    removed file that was not used

--- app/models/spree/line_item_decorator.rb
+++
@@ -1,3 +0,0 @@
-#Spree::LineItem.class_eval do
-#  has_one :spree_avalara_cart_item, :class_name => 'Spree::SpreeAvalaraCartItem', :class_name => 'Spree::SpreeAvalaraCartItem', :dependent => :destroy
-#end
