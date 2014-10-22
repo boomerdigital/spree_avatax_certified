67d0f7d58a6d67d59ae1b3d79c374f1e862ccb20
dhonig <daniel@railsdog.com>
2014-10-22 16:01:59 -0400

    update path locations

--- lib/generators/spree_avatax/install/install_generator.rb
+++ lib/generators/spree_avatax/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeAvatax
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_avatax\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_avatax\n"
+        append_file 'app/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_avatax\n"
+        append_file 'app/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_avatax\n"
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_avatax\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_avatax\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_avatax\n", :before => /\*\//, :verbose => true
+        inject_into_file 'app/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_avatax\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
@@ -17,12 +17,12 @@ module SpreeAvatax
       end
 
       def run_migrations
-         res = ask 'Would you like to run the migrations now? [Y/n]'
-         if res == '' || res.downcase == 'y'
-           run 'bundle exec rake db:migrate'
-         else
-           puts 'Skipping rake db:migrate, don\'t forget to run it!'
-         end
+        res = ask 'Would you like to run the migrations now? [Y/n]'
+        if res == '' || res.downcase == 'y'
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
       end
     end
   end
