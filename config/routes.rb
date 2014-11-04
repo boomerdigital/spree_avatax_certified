fccecb1ceede6d8eb5e04786a941987f0aa62132
Ben A Morgan <ben@benmorgan.io>
2014-11-04 14:21:58 -0800

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,8 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
-    resource :avatax_settings  do
+    resource :avatax_settings do
       get :ping_my_service, :get_file_content_txt_svc, :get_file_content_post_avatax, :get_file_content_avatax_ord
-
     end
-    resources :avalara_use_code_items do
-
-    end
-
+    resources :avalara_use_code_items
   end
- # get '/admin/log/tax_svc', :to => send_file('/log/tax_svc.txt')
-#redirect('../log/tax_svc.txt')
-
-
 end
