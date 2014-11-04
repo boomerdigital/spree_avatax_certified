40af51dbb0fafacd71c54dcffcb27b3e6ce960f5
Ben A Morgan <ben@benmorgan.io>
2014-11-04 09:03:51 -0800

    cleanup spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,11 +10,8 @@ require 'capybara/rspec'
 require 'capybara/rails'
 require 'shoulda/matchers'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
-# Requires factories defined in spree_core
 # require 'spree/testing_support/factories'
 require 'spree/testing_support/preferences'
 require 'spree/testing_support/url_helpers'
@@ -24,49 +21,26 @@ require 'spree_avatax/factories'
 
 RSpec.configure do |config|
   config.include Spree::TestingSupport::Preferences
-  config.include FactoryGirl::Syntax::Methods
-
-  # == URL Helpers
-  #
-  # Allows access to Spree's routes in specs:
-  #
-  # visit spree.admin_path
-  # current_path.should eql(spree.products_path)
   config.include Spree::TestingSupport::UrlHelpers
   config.include Spree::TestingSupport::AuthorizationHelpers
   config.include Spree::TestingSupport::ControllerRequests
+  config.include FactoryGirl::Syntax::Methods
 
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
   config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-
   config.use_transactional_fixtures = false
-  #Ensure Suite is set to use transactions for speed.
   config.before :suite do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
 
-  # Before each spec check if it is a Javascript test and switch between using database transactions or
-  #not where necessary.
   config.before :each do
     DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
     DatabaseCleaner.start
   end
 
-  # After each spec clean the database.
   config.after :each do
     DatabaseCleaner.clean
   end
