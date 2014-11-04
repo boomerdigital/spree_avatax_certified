55ae490c13c05d4283c75fcbb412dd0a882b038f
Ben A Morgan <ben@benmorgan.io>
2014-11-04 15:28:09 -0800

    refactor deface overrides

--- app/overrides/add_avalara_admin_menu.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(:virtual_path  => 'spree/admin/shared/_configuration_menu',
-                     :name          => 'add_avalara_admin_menu_link',
-                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-                     :text => %q{
-                        <%= configurations_sidebar_menu_item 'Avalara Settings', admin_avatax_settings_path%>
-                     })
