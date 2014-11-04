8764b33ffad4b425c1fc663bc1b8c516e3fe404a
Ben A Morgan <ben@benmorgan.io>
2014-11-04 14:33:25 -0800

    update to rspec 3.1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-
+ENV["RAILS_ENV"] ||= 'test'
+require 'spec_helper'
+require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'ffaker'
 require 'factory_girl'
@@ -10,13 +9,15 @@ require 'capybara/rspec'
 require 'capybara/rails'
 require 'shoulda/matchers'
 
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
-
 require 'spree/testing_support/preferences'
 require 'spree/testing_support/url_helpers'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
-require 'spree_avatax/factories'
+require 'spree_avatax_certified/factories'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include Spree::TestingSupport::Preferences
@@ -25,20 +26,7 @@ RSpec.configure do |config|
   config.include Spree::TestingSupport::ControllerRequests
   config.include FactoryGirl::Syntax::Methods
 
-  config.mock_with :rspec
-
-  config.use_transactional_fixtures = false
-
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-
-  config.before :suite do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
-  end
-  config.before :each do
-    DatabaseCleaner.start
-  end
-  config.after :each do
-    DatabaseCleaner.clean
-  end
 end
