a70e51044330dcfac68765780c0ae580b55433e9
acreilly <acreilly3@gmail.com>
2014-11-04 17:40:43 -0500

    tax svc spec working

--- spec/models/spree/tax_svc_spec.rb
+++ spec/models/spree/tax_svc_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe TaxSvc, :type => :model do
+  Spree::Config.avatax_account = "1100113096"
+  Spree::Config.avatax_license_key = "1D13EDA2DCCC7E4A"
+  Spree::Config.avatax_endpoint = "https://development.avalara.net"
+
   describe "get_tax" do
     it "gets tax" do
       taxsvc = TaxSvc.new
@@ -23,47 +27,46 @@ describe TaxSvc, :type => :model do
               :City => "Morristown",
               :PostalCode => "07960",
               :Country => "US"
-            },
-            {
-              :AddressCode => "Dest",
-              :Line1 => "73 Glenmere Drive",
-              :Line2 => "",
-              :City => "Chatham",
-              :Region => "NJ",
-              :Country => "US",
-              :PostalCode => "07928"
-            },
-            {
-              :AddressCode => "Orig",
-              :Line1 => "73 Glenmere Drive",
-              :City => "Chatham",
-              :PostalCode => "07928",
-              :Country => "United States"
-            }
-          ],
-          :Lines => [
-            {
-              :LineNo => 1,
-              :ItemCode => "ROR-00013",
-              :Qty => 3,
-              :Amount => 62.97,
-              :OriginCode => 9,
-              :DestinationCode => "Dest",
-              :Description => "Ruby on Rails Jr. Spaghetti",
-              :TaxCode => ""
-            }
-          ]
-        }
-      )
-    expect(result).to eq(something)
-    # Something is just placeholder for now until I set up VCR
+              },
+              {
+                :AddressCode => "Dest",
+                :Line1 => "73 Glenmere Drive",
+                :Line2 => "",
+                :City => "Chatham",
+                :Region => "NJ",
+                :Country => "US",
+                :PostalCode => "07928"
+                },
+                {
+                  :AddressCode => "Orig",
+                  :Line1 => "73 Glenmere Drive",
+                  :City => "Chatham",
+                  :PostalCode => "07928",
+                  :Country => "United States"
+                }
+                ],
+                :Lines => [
+                  {
+                    :LineNo => 1,
+                    :ItemCode => "ROR-00013",
+                    :Qty => 3,
+                    :Amount => 62.97,
+                    :OriginCode => 9,
+                    :DestinationCode => "Dest",
+                    :Description => "Ruby on Rails Jr. Spaghetti",
+                    :TaxCode => ""
+                  }
+                ]
+              }
+              )
+      expect(result["ResultCode"]).to eq("Success")
     end
   end
   describe "ping" do
     it "should return estimate" do
       tax_svc = TaxSvc.new
       result = tax_svc.ping
-      expect(result).to eq(something)
+      expect(result["ResultCode"]).to eq("Success")
     end
   end
 end
