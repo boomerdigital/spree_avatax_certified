41f92c643b64bb9a1bc923402c40f951c9146117
Ben A Morgan <ben@benmorgan.io>
2014-11-04 15:56:49 -0800

    slowly being refactored

--- app/models/spree/tax_svc.rb
+++ app/models/spree/tax_svc.rb
@@ -6,18 +6,10 @@ require 'rest-client'
 require 'logging'
 
 class TaxSvc
-  @@service_path = "/1.0/tax/"
-  attr_accessor :account_number
-  attr_accessor :license_key
-  attr_accessor :service_url
   logger.progname = 'tax_service'
   logger.info 'call to tax service'
 
-  def initialize
-    @account_number = Spree::Config.avatax_account
-    @license_key    = Spree::Config.avatax_license_key
-    @service_url    = Spree::Config.avatax_endpoint
-  end
+  def initialize() end
 
   def get_tax(request_hash)
     logger.info 'get_tax call'
@@ -25,12 +17,12 @@ class TaxSvc
     logger.debug JSON.generate(request_hash)
 
     begin
-      uri = @service_url + @@service_path + "get"
+      uri = service_url + "get"
       logger.debug uri
-      cred = 'Basic '+ Base64.encode64(@account_number + ":"+ @license_key)
+      cred = 'Basic '+ Base64.encode64(account_number + ":" + license_key)
       logger.debug cred
       RestClient.log = logger
-      res = RestClient.post(uri, JSON.generate(request_hash), :authorization => cred, :content_type => 'application/json'){|response, request, result| response}
+      res = response(uri, request_hash, cred)
       logger.info 'RestClient call'
       logger.debug res
       JSON.parse(res.body)
@@ -44,9 +36,9 @@ class TaxSvc
   def cancel_tax(request_hash)
     logger.info 'cancel_tax call'
     begin
-      uri = @service_url + @@service_path + "cancel"
-      cred = 'Basic '+ Base64.encode64(@account_number + ":"+ @license_key)
-      res = RestClient.post(uri, JSON.generate(request_hash), :authorization => cred, :content_type => 'application/json'){|response, request, result| response}
+      uri = service_url + "cancel"
+      cred = 'Basic '+ Base64.encode64(account_number + ":" + license_key)
+      res = response(uri, request_hash, cred)
       logger.debug res
       JSON.parse(res.body)["CancelTaxResult"]
     rescue => e
@@ -63,14 +55,14 @@ class TaxSvc
     sale_amount = 0 if sale_amount.nil?
 
     begin
-      uri = URI(@service_url + @@service_path  +
+      uri = URI(service_url +
         coordinates[:latitude].to_s + "," + coordinates[:longitude].to_s +
         "/get?saleamount=" + sale_amount.to_s )
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      cred = 'Basic '+ Base64.encode64(@account_number + ":"+ @license_key)
+      cred = 'Basic '+ Base64.encode64(account_number + ":" + license_key)
       res = http.get(uri.request_uri, 'Authorization' => cred, 'Content-Type' => 'application/json')
       JSON.parse(res.body)
     rescue => e
@@ -80,14 +72,28 @@ class TaxSvc
 
   def ping
     logger.info 'Ping Call'
-
-    self.estimate_tax(
-      { :latitude => "40.714623",
-        :longitude => "-74.006605"},
-        0 )
+    self.estimate_tax({ latitude: "40.714623", longitude: "-74.006605"}, 0)
   end
 
   private
+
+  def service_url
+    Spree::Config.avatax_endpoint + '/1.0/tax/'
+  end
+
+  def license_number
+    Spree::Config.avatax_license_key
+  end
+
+  def account_number
+    Spree::Config.avatax_account
+  end
+
+  def response(uri, request_hash, cred)
+    RestClient.post(uri, JSON.generate(request_hash), authorization: cred, content_type: 'application/json') do |response, request, result|
+      response
+    end
+  end
 
   def logger
     Logger.new('log/tax_svc.txt', 'weekly')
