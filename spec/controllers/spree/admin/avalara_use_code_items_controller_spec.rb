1686711bb113cc29fb657fe39848c7f40cbd9287
Ben A Morgan <ben@benmorgan.io>
2014-11-04 11:11:34 -0800

    whitespace

--- spec/controllers/spree/admin/avalara_use_code_items_controller_spec.rb
+++ spec/controllers/spree/admin/avalara_use_code_items_controller_spec.rb
@@ -3,9 +3,11 @@ module Spree
   module Admin
     describe AvalaraUseCodeItemsController, :type => :controller do
       stub_authorization!
+
       before :each do
         DatabaseCleaner.clean
       end
+
       describe "#index" do
         subject { spree_get :index }
 
@@ -13,37 +15,46 @@ module Spree
           expect(subject).to be_success
         end
       end
+
       describe "#show" do
         use_code = Spree::AvalaraUseCodeItem.create(use_code: "A", use_code_description: "Federal government")
         subject { spree_get :show, id: use_code.id }
+
         it "shows the use code of the id parameter" do
           expect(subject).to be_success
         end
       end
+
       describe "#new" do
         subject {spree_get :new}
+
         it "blah" do
           expect(subject).to be_success
         end
       end
+
       describe "#edit" do
         use_code = Spree::AvalaraUseCodeItem.create(use_code: "A", use_code_description: "Federal government")
         subject {spree_get :edit, id: use_code.id}
+
         it "shows edit form" do
           expect(subject).to be_success
         end
       end
 
       describe "#update" do
-          use_code = FactoryGirl.create(:use_code)
-          params = {id: use_code.id, use_code: "55"}
-          subject {spree_put :update, params}
+        use_code = FactoryGirl.create(:use_code)
+        params = {id: use_code.id, use_code: "55"}
+        subject {spree_put :update, params}
+
         it "should redirect to index path" do
           expect(subject).to redirect_to(spree.admin_avalara_use_code_items_path)
         end
+
         it "expect @use_code to eq the use_code being updated" do
           assigns(:use_code).should eq(@use_code)
         end
+
         it "should update use_code" do
           expect { subject }.to change { use_code.reload.use_code }.from('A').to('55')
         end
