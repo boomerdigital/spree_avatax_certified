90108e0e5ea81dad3385379e84b9303f97429cda
Ben A Morgan <ben@benmorgan.io>
2014-11-04 14:38:21 -0800

    remove filter configuration from rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,6 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
-
   if config.files_to_run.one?
     config.default_formatter = 'doc'
   end
