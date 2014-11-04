03a0954eecc67c54079ed3aac835b04381591205
Ben A Morgan <ben@benmorgan.io>
2014-11-04 15:02:04 -0800

    shoulda for avalara cart item

---
+++ spec/models/spree/avalara_cart_item_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Spree::AvalaraCartItem do
+  it { should belong_to :line_item }
+  it { should belong_to :avalara_transaction }
+
+  it { should validate_presence_of :index }
+  it { should validate_presence_of :tic }
+  it { should validate_presence_of :sku }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :quantity }
+
+  it { should have_db_index :line_item_id }
+  it { should have_db_index :avalara_transaction_id }
+end
