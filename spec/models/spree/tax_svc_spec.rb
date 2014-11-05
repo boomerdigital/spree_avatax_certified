e3987ac2741d790ff357693a766f671f5fd2f831
acreilly <acreilly3@gmail.com>
2014-11-05 15:55:48 -0500

    minified json

--- spec/models/spree/tax_svc_spec.rb
+++ spec/models/spree/tax_svc_spec.rb
@@ -9,55 +9,7 @@ describe TaxSvc, :type => :model do
     it "gets tax" do
       taxsvc = TaxSvc.new
       result = taxsvc.get_tax(
-        { :CustomerCode => "1",
-          :DocDate => "2014-11-03",
-          :CompanyCode => "54321",
-          :CustomerUsageType => "",
-          :ExemptionNo => nil,
-          :Client => "SpreeExtV1.0",
-          :DocCode => "R731071205",
-          :ReferenceCode => "R731071205",
-          :DetailLevel => "Tax",
-          :Commit => false,
-          :DocType => "SalesInvoice",
-          :Addresses => [
-            {
-              :AddressCode => 9,
-              :Line1 => "31 South St",
-              :City => "Morristown",
-              :PostalCode => "07960",
-              :Country => "US"
-              },
-              {
-                :AddressCode => "Dest",
-                :Line1 => "73 Glenmere Drive",
-                :Line2 => "",
-                :City => "Chatham",
-                :Region => "NJ",
-                :Country => "US",
-                :PostalCode => "07928"
-                },
-                {
-                  :AddressCode => "Orig",
-                  :Line1 => "73 Glenmere Drive",
-                  :City => "Chatham",
-                  :PostalCode => "07928",
-                  :Country => "United States"
-                }
-                ],
-                :Lines => [
-                  {
-                    :LineNo => 1,
-                    :ItemCode => "ROR-00013",
-                    :Qty => 3,
-                    :Amount => 62.97,
-                    :OriginCode => 9,
-                    :DestinationCode => "Dest",
-                    :Description => "Ruby on Rails Jr. Spaghetti",
-                    :TaxCode => ""
-                  }
-                ]
-              }
+        {:CustomerCode=>"1",:DocDate=>"2014-11-03",:CompanyCode=>"54321",:CustomerUsageType=>"",:ExemptionNo=>nil,:Client=>"SpreeExtV1.0",:DocCode=>"R731071205",:ReferenceCode=>"R731071205",:DetailLevel=>"Tax",:Commit=>false,:DocType=>"SalesInvoice",:Addresses=>[{:AddressCode=>9,:Line1=>"31 South St",:City=>"Morristown",:PostalCode=>"07960",:Country=>"US"},{:AddressCode=>"Dest",:Line1=>"73 Glenmere Drive",:Line2=>"",:City=>"Chatham",:Region=>"NJ",:Country=>"US",:PostalCode=>"07928"},{:AddressCode=>"Orig",:Line1=>"73 Glenmere Drive",:City=>"Chatham",:PostalCode=>"07928",:Country=>"United States"}],:Lines=>[{:LineNo=>1,:ItemCode=>"ROR-00013",:Qty=>3,:Amount=>62.97,:OriginCode=>9,:DestinationCode=>"Dest",:Description=>"Ruby on Rails Jr. Spaghetti",:TaxCode=>""}]}
               )
       expect(result["ResultCode"]).to eq("Success")
     end
