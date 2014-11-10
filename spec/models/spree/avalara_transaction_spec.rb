797c14299153016d3ffeba2148ae76e70336e22d
acreilly <acreilly3@gmail.com>
2014-11-10 16:40:43 -0500

    added more tests

--- spec/models/spree/avalara_transaction_spec.rb
+++ spec/models/spree/avalara_transaction_spec.rb
@@ -4,6 +4,7 @@ describe Spree::AvalaraTransaction, :type => :model do
 
   it { should belong_to :order }
   it { should belong_to :return_authorization }
+  # it { should have_one :adjustment }
   it { should validate_presence_of :order }
   it { should have_db_index :order_id }
 
@@ -36,14 +37,16 @@ describe Spree::AvalaraTransaction, :type => :model do
   end
 
   describe "commit_avatax" do
-
+    it "should commit avatax" do
+      @order.avalara_capture
+      expect(@order.avalara_transaction.commit_avatax(@order.line_items, @order)).to eq("0.4")
+    end
   end
 
   describe "commit_avatax_final" do
-
-  end
-
-  describe "update_adjustment" do
-
+    it "should commit avatax final" do
+      @order.avalara_capture
+      expect(@order.avalara_transaction.commit_avatax_final(@order.line_items, @order, @order.number.to_s + ":" + @order.id.to_s, @order.completed_at)).to eq("0.4")
+    end
   end
 end
