ba0812c41812c14b1052c781e76d8b1fda471102
acreilly <acreilly3@gmail.com>
2014-11-10 17:13:44 -0500

    removed white space

--- app/models/spree/spree_avalara_cart_item.rb
+++ app/models/spree/spree_avalara_cart_item.rb
@@ -2,17 +2,10 @@ require 'builder'
 
 module Spree
   class SpreeAvalaraCartItem < ActiveRecord::Base
-
-
-
     belongs_to :line_item
-
     belongs_to :avalara_transaction
 
-
-
     validates :index, :tic, :sku, :price, :quantity, :presence => true
-
     accepts_nested_attributes_for :line_item
 
     def to_hash
