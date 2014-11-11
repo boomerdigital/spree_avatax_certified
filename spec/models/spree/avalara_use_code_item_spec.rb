4aeadd8fabef66b4ea8a82b315cc8fef78c76625
acreilly <acreilly3@gmail.com>
2014-11-11 14:05:03 -0500

    all specs passing

--- spec/models/spree/avalara_use_code_item_spec.rb
+++ spec/models/spree/avalara_use_code_item_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Spree::AvalaraUseCodeItem, type: :model do
-  it { should belong_to :user }
+  it { should have_many :users }
 end
--- spec/models/spree/user_decorator_spec.rb
+++ spec/models/spree/user_decorator_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe Spree.user_class do
-  it { should have_one :avalara_use_code_item }
+  it { should belong_to :avalara_use_code_item }
 end
