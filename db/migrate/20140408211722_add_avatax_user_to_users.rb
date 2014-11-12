8bc683aa1301142efc6a8d2b65daab4537337dc3
acreilly <acreilly3@gmail.com>
2014-11-12 16:21:47 -0500

    removed customer code column since it is just the user's id

--- db/migrate/20140408211722_add_avatax_user_to_users.rb
+++ db/migrate/20140408211722_add_avatax_user_to_users.rb
@@ -1,6 +1,5 @@
 class AddAvataxUserToUsers < ActiveRecord::Migration
   def change
-    add_column :spree_users, :customer_code, :string
     add_column :spree_users, :exemption_number, :string
   end
 end
