8090354f453011aa745c9e5c6a44fee05ef63690
acreilly <acreilly3@gmail.com>
2014-11-12 16:24:07 -0500

    changed to if there is no customer code use work Guest

--- app/models/spree/avalara_transaction.rb
+++ app/models/spree/avalara_transaction.rb
@@ -321,7 +321,7 @@ module Spree
       addresses<<orig_address
 
       gettaxes = {
-        :CustomerCode => Spree::Config.avatax_customer_code, # COME BACK TO THIS!
+        :CustomerCode => myuser ? myuser.id : "Guest",
         :DocDate => org_ord_date ? org_ord_date : Date.current.to_formatted_s(:db),
 
         :CompanyCode => Spree::Config.avatax_company_code,
