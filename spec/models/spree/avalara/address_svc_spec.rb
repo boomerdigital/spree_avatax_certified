db8adc3d908494d339e5f306b69fb23562110d47
acreilly <acreilly3@gmail.com>
2014-11-12 19:54:29 -0500

    set up address spec

---
+++ spec/models/spree/avalara/address_svc_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe AddressSvc, :type => :model do
+ describe "#validate" do
+    xit "validates address with success" do
+
+    end
+
+    xit "does not validate address" do
+
+    end
+ end
+end
