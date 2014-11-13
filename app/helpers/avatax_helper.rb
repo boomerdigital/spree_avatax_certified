3ec2f0fe59cd8c38f685b4fc0301699b432d85ed
acreilly <acreilly3@gmail.com>
2014-11-13 15:26:41 -0500

    added logger_enabled? so logging only occurs when enabled

--- app/helpers/avatax_helper.rb
+++ app/helpers/avatax_helper.rb
@@ -11,28 +11,40 @@ module AvataxHelper
       @logger
     end
 
+    def logger_enabled?
+      Spree::Config.avatax_log
+    end
+
     def progname(progname = nil)
-      progname.nil? ? logger.progname : logger.progname = progname
+      if logger_enabled?
+        progname.nil? ? logger.progname : logger.progname = progname
+      end
     end
 
     def info(log_info = nil)
-      logger.info log_info unless log_info.nil?
+      if logger_enabled?
+        logger.info log_info unless log_info.nil?
+      end
     end
 
     def info_and_debug(log_info, request_hash)
-      logger.info log_info
-      logger.debug request_hash
-      logger.debug JSON.generate(request_hash)
+      if logger_enabled?
+        logger.info log_info
+        logger.debug request_hash
+        logger.debug JSON.generate(request_hash)
+      end
     end
 
 
     def debug(error, text = nil)
-      logger.debug error
-      if text.nil?
-        error
-      else
-        logger.debug text
-        text
+      if logger_enabled?
+        logger.debug error
+        if text.nil?
+          error
+        else
+          logger.debug text
+          text
+        end
       end
     end
   end
