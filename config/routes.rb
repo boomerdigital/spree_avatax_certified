54c624ae21b6b4f6696f8da1036d6fca030e289c
acreilly <acreilly3@gmail.com>
2014-11-14 13:01:29 -0500

    added validate address button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
     resource :avatax_settings do
-      get :ping_my_service, :get_file_txt_tax_svc, :get_file_post_order_to_avalara, :get_file_avalara_order, :erase_data
+      get :ping_my_service, :get_file_txt_tax_svc, :get_file_post_order_to_avalara, :get_file_avalara_order, :erase_data, :validate_address
     end
     resources :avalara_entity_use_codes do
 
