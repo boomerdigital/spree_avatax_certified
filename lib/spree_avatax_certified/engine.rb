1501ac4907b17950e0f051d2601add27405ac6d9
acreilly <acreilly3@gmail.com>
2014-12-26 17:57:46 -0500

    moving config preferences to engine

--- lib/spree_avatax_certified/engine.rb
+++ lib/spree_avatax_certified/engine.rb
@@ -11,6 +11,23 @@ module SpreeAvataxCertified
       g.test_framework :rspec
     end
 
+    initializer "spree.avatax_certified.preferences", :before => :load_config_initializers do |app|
+      Spree::AppConfiguration.class_eval do
+        preference :avatax_api_username, :string
+        preference :avatax_api_password, :string
+        preference :avatax_company_code, :string
+        preference :avatax_endpoint, :string
+        preference :avatax_account, :string
+        preference :avatax_license_key, :string
+        preference :avatax_iseligible, :boolean, default: true
+        preference :avatax_log, :boolean, default: true
+        preference :avatax_address_validation, :boolean, default: true
+        preference :avatax_tax_calculation, :boolean, default: true
+        preference :avatax_document_commit, :boolean, default: true
+        preference :avatax_origin, :string, default: "{}"
+      end
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/models/**/*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
