596d30734551dad700df2be09d2cd8d2ee2e86b9
acreilly <acreilly3@gmail.com>
2014-12-29 20:37:41 -0500

    created page for avalar info on a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Spree::Core::Engine.routes.draw do
   namespace :admin do
+    resources :users do
+      member do
+        get :avalara_information
+      end
+    end
     resource :avatax_settings do
       get :ping_my_service, :get_file_txt_tax_svc, :get_file_post_order_to_avalara, :get_file_avalara_order, :erase_data, :validate_address
     end
