bd2f6c3964f17e2e0ca5a19fbb7ea7d1d42d277e
Eric Saupe <ericsaupe@gmail.com>
2015-01-14 09:22:28 -0700

    Better handling of calculated adjustments on line items

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -10,4 +10,17 @@ Spree::LineItem.class_eval do
       'TaxCategory' => tax_category
     }
   end
+
+  def update_adjustments
+    if quantity_changed?
+      update_tax_charge # Called to ensure pre_tax_amount is updated.
+      recalculate_adjustments
+      update_avalara_tax
+    end
+  end
+
+  def update_avalara_tax
+    tax = self.adjustments.avalara_tax.sum(:amount)
+    self.update(adjustment_total: tax)
+  end
 end
--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -33,10 +33,7 @@ Spree::Order.class_eval do
 
     begin
       create_avalara_transaction
-      self.adjustments.avalara_tax.destroy_all
-      line_items.each do |l|
-        l.adjustments.avalara_tax.destroy_all
-      end
+      self.all_adjustments.avalara_tax.destroy_all
       @rtn_tax = self.avalara_transaction.commit_avatax(line_items, self)
 
       logger.info 'tax amount'
@@ -70,8 +67,11 @@ Spree::Order.class_eval do
           adjustment.order = self
         end
       end
+      line_items.each do |l|
+        l.update_avalara_tax
+      end
       self.reload.update!
-      adjustments.avalara_tax.last
+      all_adjustments.avalara_tax
     rescue => e
       logger.debug e
       logger.debug 'error in a avalara capture'
@@ -83,17 +83,13 @@ Spree::Order.class_eval do
     begin
       create_avalara_transaction
 
-      self.adjustments.avalara_tax.destroy_all
-      line_items.each do |l|
-        l.adjustments.avalara_tax.destroy_all
-      end
+      self.all_adjustments.avalara_tax.destroy_all
       @rtn_tax = self.avalara_transaction.commit_avatax_final(line_items, self)
 
       logger.info 'tax amount'
       logger.debug @rtn_tax
 
       order_tax = 0
-
       @rtn_tax["TaxLines"].each do |tax_line|
         if !tax_line["LineNo"].include? "-"
           line_item = Spree::LineItem.find(tax_line["LineNo"])
@@ -120,8 +116,11 @@ Spree::Order.class_eval do
           adjustment.order = self
         end
       end
+      line_items.each do |l|
+        l.update_avalara_tax
+      end
       self.reload.update!
-      adjustments.avalara_tax.last
+      all_adjustments.avalara_tax
     rescue => e
       logger.debug e
       logger.debug 'error in a avalara capture finalize'
@@ -130,7 +129,7 @@ Spree::Order.class_eval do
 
   def display_avalara_tax_total
     avatax_tax = BigDecimal.new("0")
-    self.adjustments.avalara_tax.each do |tax|
+    self.all_adjustments.avalara_tax.each do |tax|
       avatax_tax += tax.amount
     end
     Spree::Money.new(avatax_tax, { currency: currency })
