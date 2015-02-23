1c06bcd13177f1abf4bdab47e8455835cd703cee
acreilly <acreilly3@gmail.com>
2015-02-23 16:19:01 -0500

    added view and scope for avalara tax

--- app/models/spree/adjustment_decorator.rb
+++ app/models/spree/adjustment_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Adjustment.class_eval do
-  scope :tax, -> { where("source_type = 'Spree::TaxRate' OR source_type = 'Spree::AvalaraTransaction'") }
+  scope :all_tax, -> { where("source_type = 'Spree::TaxRate' OR source_type = 'Spree::AvalaraTransaction'") }
   scope :avalara_tax, -> { where(source_type: 'Spree::AvalaraTransaction') }
   scope :not_avalara_tax, -> { where.not(source_type: 'Spree::AvalaraTransaction') }
 end
