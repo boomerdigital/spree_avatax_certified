97ac4594bb64bb041c0685f3bb198c6d8eba66b9
acreilly <acreilly3@gmail.com>
2015-03-27 11:19:17 -0400

    moved address and tax svc out of spree folder

--- spec/models/spree/avalara_transaction_spec.rb
+++ spec/models/spree/avalara_transaction_spec.rb
@@ -6,7 +6,7 @@ describe Spree::AvalaraTransaction, :type => :model do
   it { should belong_to :return_authorization }
   it { should validate_presence_of :order }
   it { should have_db_index :order_id }
-  it { should have_one :adjustment }
+  it { should have_many :adjustments }
 
   before :each do
     MyConfigPreferences.set_preferences
