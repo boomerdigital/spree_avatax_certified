3ae00bd9f95c788b15fc314ea79f4d8b2a2bec60
acreilly <acreilly3@gmail.com>
2015-04-14 10:55:22 -0400

    fixed admin nav, changed version to be lastest version

--- app/overrides/spree/admin/shared/_configuration_menu.rb
+++ app/overrides/spree/admin/shared/_configuration_menu.rb
@@ -4,7 +4,7 @@ Deface::Override.new(
   insert_bottom: "[data-hook='admin_configurations_sidebar_menu']"
 ) do
   <<-HTML
-    <%= configurations_sidebar_menu_item Spree.t('avalara.settings'), admin_avatax_settings_path %>
-    <%= configurations_sidebar_menu_item Spree.t('avalara_entity_use_code_settings'), admin_avalara_entity_use_codes_path %>
+    <%= configurations_sidebar_menu_item Spree.t('avalara.settings'), spree.admin_avatax_settings_path %>
+    <%= configurations_sidebar_menu_item Spree.t('avalara_entity_use_code_settings'), spree.admin_avalara_entity_use_codes_path %>
   HTML
 end
