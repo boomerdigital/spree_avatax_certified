fc61e965a8b356170f24b04cb3ac05081dcd9e8d
acreilly <acreilly3@gmail.com>
2015-06-15 16:45:00 -0400

    refactored to use method amount

--- app/models/spree/avalara_transaction.rb
+++ app/models/spree/avalara_transaction.rb
@@ -292,7 +292,7 @@ module Spree
             line[:LineNo] = line_item.id
             line[:ItemCode] = line_item.variant.sku
             line[:Qty] = line_item.quantity
-            line[:Amount] = line_item.price.to_f * line_item.quantity
+            line[:Amount] = line_item.amount.to_f
             line[:OriginCode] = "Orig"
             line[:DestinationCode] = "Dest"
 
