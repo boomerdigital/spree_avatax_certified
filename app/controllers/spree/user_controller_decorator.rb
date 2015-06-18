6a655c47e7a66852458972b4a0a7e59ba51e1a84
acreilly <acreilly3@gmail.com>
2015-06-18 13:37:48 -0400

    removed user decorator

--- app/controllers/spree/user_controller_decorator.rb
+++
@@ -1,6 +0,0 @@
-Spree::Admin::UsersController.class_eval do
-  def user_params
-    params.require(:user).permit(permitted_user_attributes)
-    params.require(:user).permit(:exemption_number, :avalara_entity_use_code_id)
-  end
-end
