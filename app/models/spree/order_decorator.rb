3db885102e5689e9b46e32dccd745934c6d13f23
acreilly <acreilly3@gmail.com>
2015-07-29 16:47:10 -0400

    fixed order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -4,7 +4,7 @@ Spree::Order.class_eval do
 
   has_one :avalara_transaction, dependent: :destroy
 
-  self.state_machine.before_transition :to => :complete,
+  self.state_machine.after_transition :to => :complete,
                                       :do => :avalara_capture_finalize,
                                       :if => :avalara_eligible
 
@@ -22,10 +22,6 @@ Spree::Order.class_eval do
     :lookup_avatax
   end
 
-  def rtn_tax
-    @rtn_tax
-  end
-
   def cancel_status
     return nil unless avalara_transaction.present?
     self.avalara_transaction.check_status(self)
@@ -36,48 +32,45 @@ Spree::Order.class_eval do
 
     begin
       create_avalara_transaction
-      self.all_adjustments.avalara_tax.destroy_all
       self.line_items.reload
 
-      @rtn_tax = self.avalara_transaction.commit_avatax(line_items, self, self.number.to_s, Date.today.strftime("%F"), "SalesInvoice")
+      @rtn_tax = self.avalara_transaction.commit_avatax(line_items, self, self.number.to_s, Date.today.strftime('%F'), 'SalesInvoice')
 
       logger.info 'tax amount'
       logger.debug @rtn_tax
       @rtn_tax
     rescue => e
       logger.debug e
-      logger.debug 'error in a avalara capture'
+      logger.debug 'error in avalara capture'
     end
   end
 
   def avalara_capture_finalize
     logger.debug 'avalara capture finalize'
-
     begin
       create_avalara_transaction
-      self.all_adjustments.avalara_tax.destroy_all
       self.line_items.reload
-      @rtn_tax = self.avalara_transaction.commit_avatax_final(line_items, self, self.number.to_s, Date.today.strftime("%F"), "SalesInvoice")
+      @rtn_tax = self.avalara_transaction.commit_avatax_final(line_items, self, self.number.to_s, Date.today.strftime('%F'), 'SalesInvoice')
 
       logger.info 'tax amount'
       logger.debug @rtn_tax
       @rtn_tax
     rescue => e
       logger.debug e
-      logger.debug 'error in a avalara capture finalize'
+      logger.debug 'error in avalara capture finalize'
     end
   end
 
-  def display_avalara_tax_total
-    avatax_tax = BigDecimal.new("0")
-    self.all_adjustments.avalara_tax.each do |tax|
-      avatax_tax += tax.amount
-    end
-    Spree::Money.new(avatax_tax, { currency: currency })
+  def avatax_cache_key
+    key = ["Spree::Order"]
+    key << self.number
+    key << self.promo_total
+    key.join("-")
   end
 
   private
+
   def logger
-    @logger ||= AvataxHelper::AvataxLog.new("avalara_order", "order class", 'start order processing')
+    @logger ||= AvataxHelper::AvataxLog.new('avalara_order', 'order class', 'start order processing')
   end
 end
