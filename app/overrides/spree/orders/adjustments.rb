de4b23313c1d5ca13b4ccee7f173d663bfa0cf12
acreilly <acreilly3@gmail.com>
2015-07-29 15:02:16 -0400

    deleting unused views

--- app/overrides/spree/orders/adjustments.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/orders/_adjustments',
-  name:         'added avalara tax adjustment to cart',
-  insert_top:   '#cart_adjustments',
-  partial: "spree/shared/avalara_cart_adjustment"
-)
