8169aded4fa8cb2163e09e9fd9fdf30014d35cfc
acreilly <acreilly3@gmail.com>
2015-07-29 16:10:14 -0400

    fixing cache keys

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -12,10 +12,11 @@ Spree::LineItem.class_eval do
   end
 
   def avatax_cache_key
-    key = ["Spree::LineItem"]
+    key = ['Spree::LineItem']
     key << self.id
     key << self.quantity
-    key << self.cost_price
+    key << self.price
+    key << self.promo_total
     key.join("-")
   end
 end
