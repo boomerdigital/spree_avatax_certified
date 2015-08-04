e0a3218e6d11f69af9ca907cdd16bb2e408eed7b
Daniel J. Pritchett <dpritchett@gmail.com>
2015-08-04 14:44:28 -0500

    Attempting to force versioning and changelogging into the 3.0 calc PR

--- config/initializers/avatax.rb
+++ config/initializers/avatax.rb
@@ -1,3 +1,5 @@
-AVATAX_CLIENT_VERSION = 'SpreeExtV2.4'
+gem_version = File.read(File.expand_path('../../../GEM_VERSION',__FILE__)).strip
+
+AVATAX_CLIENT_VERSION = "SpreeExtV#{gem_version}"
 AVATAX_SERVICEPATH_ADDRESS = '/1.0/address/'
 AVATAX_SERVICEPATH_TAX = '/1.0/tax/'
