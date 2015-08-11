aca3862eae51bc8c7a8e9948f2fd713ead8b736a
acreilly <acreilly3@gmail.com>
2015-08-11 12:56:08 -0400

    last of styling fixes

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,13 +3,13 @@ require 'logger'
 Spree::Order.class_eval do
   has_one :avalara_transaction, dependent: :destroy
 
-  self.state_machine.after_transition :to => :complete,
-                                      :do => :avalara_capture_finalize,
-                                      :if => :avalara_eligible
+  self.state_machine.after_transition to: :complete,
+                                      do: :avalara_capture_finalize,
+                                      if: :avalara_eligible
 
- self.state_machine.before_transition :to => :canceled,
-                                      :do => :cancel_status,
-                                      :if => :avalara_eligible
+ self.state_machine.before_transition to: :canceled,
+                                      do: :cancel_status,
+                                      if: :avalara_eligible
 
   def avalara_eligible
     Spree::Config.avatax_iseligible
@@ -23,7 +23,7 @@ Spree::Order.class_eval do
 
   def cancel_status
     return nil unless avalara_transaction.present?
-    self.avalara_transaction.check_status(self)
+    avalara_transaction.check_status(self)
   end
 
   def avalara_capture
@@ -31,9 +31,9 @@ Spree::Order.class_eval do
 
     begin
       create_avalara_transaction
-      self.line_items.reload
+      line_items.reload
 
-      @rtn_tax = self.avalara_transaction.commit_avatax(line_items, self, self.number.to_s, Date.today.strftime('%F'), 'SalesInvoice')
+      @rtn_tax = avalara_transaction.commit_avatax(line_items, self, number.to_s, Date.today.strftime('%F'), 'SalesInvoice')
 
       logger.info 'tax amount'
       logger.debug @rtn_tax
@@ -48,8 +48,8 @@ Spree::Order.class_eval do
     logger.debug 'avalara capture finalize'
     begin
       create_avalara_transaction
-      self.line_items.reload
-      @rtn_tax = self.avalara_transaction.commit_avatax_final(line_items, self, self.number.to_s, Date.today.strftime('%F'), 'SalesInvoice')
+      line_items.reload
+      @rtn_tax = avalara_transaction.commit_avatax_final(line_items, self, number.to_s, Date.today.strftime('%F'), 'SalesInvoice')
 
       logger.info 'tax amount'
       logger.debug @rtn_tax
