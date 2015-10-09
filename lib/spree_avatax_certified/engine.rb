4e23d6c33fa502b1eac2af31c3d2aa6d71f642d7
acreilly <acreilly3@gmail.com>
2015-10-09 12:40:29 -0400

    2-3 uses United States without of america

--- lib/spree_avatax_certified/engine.rb
+++ lib/spree_avatax_certified/engine.rb
@@ -21,7 +21,7 @@ module SpreeAvataxCertified
         preference :avatax_iseligible, :boolean, default: true
         preference :avatax_log, :boolean, default: true
         preference :avatax_address_validation, :boolean, default: true
-        preference :avatax_address_validation_enabled_countries, :array, default: ["United States of America", "Canada"]
+        preference :avatax_address_validation_enabled_countries, :array, default: ["United States", "Canada"]
         preference :avatax_tax_calculation, :boolean, default: true
         preference :avatax_document_commit, :boolean, default: true
         preference :avatax_origin, :string, default: "{}"
