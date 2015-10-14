7e3c88ed22c3686006cc77b399c4cbea2ee82d49
Allie <acreilly3@gmail.com>
2015-10-14 16:37:07 -0400

    fixed payment decorator to handle voids and also alter payment amount if tax is there

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,10 +1,24 @@
 Spree::Payment.class_eval do
-  self.state_machine.after_transition to: :completed,
-                                      do: :avalara_finalize
+  self.state_machine.before_transition to: :completed,
+  do: :avalara_finalize
+  self.state_machine.after_transition to: :void, do: :cancel_avalara
 
-  # create validation that makes sure payment captured is of full amount that was sent to avalara if the order has tax
+  def avatax_eligible
+    Spree::Config.avatax_iseligible
+  end
+
+  def cancel_avalara
+    order.avalara_transaction.cancel_order unless order.avalara_transaction.nil?
+  end
 
   def avalara_finalize
-    order.avalara_capture_finalize if Spree::Config.avatax_iseligible
+    if order.additional_tax_total > 0
+      if self.amount != order.total
+        self.update_attributes(amount: order.total)
+        order.avalara_capture_finalize if avatax_eligible
+      else
+        order.avalara_capture_finalize if avatax_eligible
+      end
+    end
   end
 end
