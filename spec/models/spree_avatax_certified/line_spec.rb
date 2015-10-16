81beb257a87373929e32b250eca8a5226fb49113
Allison C. Reilly <acreilly3@gmail.com>
2015-10-16 11:50:35 -0400

    refactored line model and added more specs

---
+++ spec/models/spree_avatax_certified/line_spec.rb
@@ -0,0 +1,83 @@
+require 'spec_helper'
+
+describe SpreeAvataxCertified::Line, :type => :model do
+  let(:country){ FactoryGirl.create(:country) }
+  let(:address){ FactoryGirl.create(:address) }
+  let(:order) { FactoryGirl.create(:order_with_line_items) }
+
+  before do
+    order.ship_address.update_attributes(city: 'Tuscaloosa', address1: '220 Paul W Bryant Dr')
+  end
+
+  let(:sales_lines) { SpreeAvataxCertified::Line.new(order, 'SalesOrder') }
+  let(:return_lines) { SpreeAvataxCertified::Line.new(order, 'ReturnOrder') }
+
+  describe '#initialize' do
+    it 'should have order' do
+      expect(sales_lines.order).to eq(order)
+    end
+    it 'should have invoice_type' do
+      expect(sales_lines.invoice_type).to eq('SalesOrder')
+    end
+    it 'should have lines be an array' do
+      expect(sales_lines.lines).to be_kind_of(Array)
+    end
+    it 'lines should be a length of 5' do
+      expect(sales_lines.lines.length).to eq(5)
+    end
+    it 'should have stock locations' do
+      expect(sales_lines.stock_locations).to eq(sales_lines.order_stock_locations)
+    end
+  end
+
+  context 'sales order' do
+    describe '#build_lines' do
+      it 'receives method item_lines_array' do
+        expect(sales_lines).to receive(:item_lines_array)
+        sales_lines.build_lines
+      end
+      it 'receives method shipment_lines_array' do
+        expect(sales_lines).to receive(:shipment_lines_array)
+        sales_lines.build_lines
+      end
+    end
+
+    describe '#item_lines_array' do
+      it 'returns an Array' do
+        expect(sales_lines.item_lines_array).to be_kind_of(Array)
+      end
+    end
+
+    describe '#shipment_lines_array' do
+      it 'returns an Array' do
+        expect(sales_lines.shipment_lines_array).to be_kind_of(Array)
+      end
+      it 'should have a length of 1' do
+        expect(sales_lines.shipment_lines_array.length).to eq(1)
+      end
+    end
+
+    describe '#item_line' do
+      it 'returns a Hash' do
+        expect(sales_lines.item_line(order.line_items.first)).to be_kind_of(Hash)
+      end
+    end
+    describe '#shipment_line' do
+      it 'returns a Hash' do
+        expect(sales_lines.shipment_line(order.shipments.first)).to be_kind_of(Hash)
+      end
+    end
+  end
+
+  context 'return invoice' do
+    describe 'build_lines' do
+      it 'receives method return_authorization_lines' do
+        expect(return_lines).to receive(:return_authorization_lines)
+        return_lines.build_lines
+      end
+    end
+    describe '#return_authorization_lines' do
+
+    end
+  end
+end
