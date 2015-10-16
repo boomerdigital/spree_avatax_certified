94c6474c8047e8c1a23bed246451f172f410b424
Allison C. Reilly <acreilly3@gmail.com>
2015-10-16 14:54:51 -0400

    refactored code

--- app/models/spree/return_authorization_decorator.rb
+++ app/models/spree/return_authorization_decorator.rb
@@ -4,11 +4,11 @@ RETURN_AUTHORIZATION_LOGGER = AvataxHelper::AvataxLog.new('return_authorization'
 Spree::ReturnAuthorization.class_eval do
   RETURN_AUTHORIZATION_LOGGER.info('start ReturnAuthorization processing')
 
-  has_one :avalara_transaction, dependent: :destroy
-  after_save :assign_avalara_transaction, on: :create
+  has_one :avalara_transaction
+  after_save :assign_avalara_transaction, if: :order_has_avalara_transaction?
   self.state_machine.before_transition :to => :received,
-                                      :do => :avalara_capture_finalize,
-                                      :if => :avalara_eligible
+                                       :do => :avalara_capture_finalize,
+                                       :if => :avalara_eligible
 
   def avalara_eligible
     Spree::Config.avatax_iseligible
@@ -24,9 +24,10 @@ Spree::ReturnAuthorization.class_eval do
     RETURN_AUTHORIZATION_LOGGER.debug 'avalara capture return_authorization'
 
     begin
-      @rtn_tax = Spree::AvalaraTransaction.find_by_return_authorization_id(self.id).commit_avatax('ReturnInvoice')
+      @rtn_tax = self.avalara_transaction.commit_avatax('ReturnInvoice')
 
       RETURN_AUTHORIZATION_LOGGER.info_and_debug('tax amount', @rtn_tax)
+      @rtn_tax
     rescue => e
       RETURN_AUTHORIZATION_LOGGER.debug e
       RETURN_AUTHORIZATION_LOGGER.debug 'error in a avalara capture return_authorization'
@@ -40,6 +41,7 @@ Spree::ReturnAuthorization.class_eval do
       @rtn_tax = self.avalara_transaction.commit_avatax_final('ReturnInvoice')
 
       RETURN_AUTHORIZATION_LOGGER.info_and_debug('tax amount', @rtn_tax)
+      @rtn_tax
     rescue => e
       RETURN_AUTHORIZATION_LOGGER.debug e
       RETURN_AUTHORIZATION_LOGGER.debug 'error in a avalara capture return_authorization'
@@ -52,11 +54,13 @@ Spree::ReturnAuthorization.class_eval do
 
   def assign_avalara_transaction
     if avalara_eligible
-      if self.avalara_transaction.nil?
-        create_avalara_transaction_return_auth
-      else
-        Spree::AvalaraTransaction.find_by_return_authorization_id(self.id).update_attributes(order_id: order.id, return_authorization_id: self.id)
+      if order.avalara_transaction.return_authorization_id.nil?
+        Spree::AvalaraTransaction.find_by_order_id(order.id).update_attributes(return_authorization_id: self.id)
       end
     end
+  end
+
+  def order_has_avalara_transaction?
+    order.avalara_transaction.nil? ? false : true
   end
 end
