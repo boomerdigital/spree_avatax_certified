69d680c12301370bb273a728216e3ee2e034c11a
Allison C. Reilly <acreilly3@gmail.com>
2015-10-16 17:25:25 -0500

    refactoring order spec

--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -4,32 +4,27 @@ describe Spree::Order, type: :model do
 
   it { should have_one :avalara_transaction }
 
-  let(:order_with_line_items) {FactoryGirl.create(:order_with_line_items)}
+  let(:order) {FactoryGirl.create(:order_with_line_items)}
   let(:variant) { create(:variant) }
 
-  before :each do
+  before do
     MyConfigPreferences.set_preferences
     stock_location = FactoryGirl.create(:stock_location)
-    @order = FactoryGirl.create(:order)
-    line_item = FactoryGirl.create(:line_item)
-    line_item.tax_category.update_attributes(name: "Clothing", description: "PC030000")
-    @order.line_items << line_item
-    to_address = Spree::Address.create(firstname: "Allison", lastname: "Reilly", address1: "220 Paul W Bryant Dr", city: "Tuscaloosa", zipcode: "35401", phone: "9733492462", state_name: "Alabama", state_id: 39, country_id: 1)
-    @order.update_attributes(ship_address: to_address, bill_address: to_address)
+    order.line_items.first.tax_category.update_attributes(name: "Clothing", description: "PC030000")
   end
 
   describe "#avalara_eligible" do
     it "should return true" do
-      expect(@order.avalara_eligible).to eq(true)
+      expect(order.avalara_eligible).to eq(true)
     end
   end
 
   describe "#avalara_lookup" do
     it "should return lookup_avatax" do
-      expect(@order.avalara_lookup).to eq(:lookup_avatax)
+      expect(order.avalara_lookup).to eq(:lookup_avatax)
     end
     it "creates new avalara_transaction" do
-      expect{@order.avalara_lookup}.to change{Spree::AvalaraTransaction.count}.by(1)
+      expect{order.avalara_lookup}.to change{Spree::AvalaraTransaction.count}.by(1)
     end
   end
 
@@ -69,27 +64,27 @@ describe Spree::Order, type: :model do
 
   describe "#avalara_capture" do
     it "should response with Hash object" do
-      expect(@order.avalara_capture).to be_kind_of(Hash)
+      expect(order.avalara_capture).to be_kind_of(Hash)
     end
     it "creates new avalara_transaction" do
-      expect{@order.avalara_capture}.to change{Spree::AvalaraTransaction.count}.by(1)
+      expect{order.avalara_capture}.to change{Spree::AvalaraTransaction.count}.by(1)
     end
   end
 
   describe "#avalara_capture_finalize" do
     it "should response with Hash object" do
-      expect(@order.avalara_capture_finalize).to be_kind_of(Hash)
+      expect(order.avalara_capture_finalize).to be_kind_of(Hash)
     end
     it "creates new avalara_transaction" do
-      expect{@order.avalara_capture_finalize}.to change{Spree::AvalaraTransaction.count}.by(1)
+      expect{order.avalara_capture_finalize}.to change{Spree::AvalaraTransaction.count}.by(1)
     end
   end
 
   describe '#avatax_cache_key' do
     it 'should respond with a cache key' do
-      expected_response = "Spree::Order-#{@order.number}-#{@order.promo_total}"
+      expected_response = "Spree::Order-#{order.number}-#{order.promo_total}"
 
-      expect(@order.avatax_cache_key).to eq(expected_response)
+      expect(order.avatax_cache_key).to eq(expected_response)
     end
   end
 end
