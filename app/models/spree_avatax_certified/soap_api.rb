ae312d4bd9c8acd1ea31195ab16efb140c762f11
Allison C. Reilly <acreilly3@gmail.com>
2016-02-04 14:11:03 -0500

    refactored avalara eligible
    refactored

--- app/models/spree_avatax_certified/soap_api.rb
+++ app/models/spree_avatax_certified/soap_api.rb
@@ -2,36 +2,56 @@ require 'savon'
 
 module SpreeAvataxCertified
   class SoapApi
+    attr_reader :spec
     def initialize
-      @username = Spree::Config.avatax_account
-      @password = Spree::Config.avatax_license_key
-      @service_url = Spree::Config.avatax_endpoint
-      @clientname = AVATAX_CLIENT_VERSION
-
-      spec = Gem::Specification.find_by_name('spree_avatax_certified')
-      @name = 'test' # remember to change this: Reference to a named profile
-      @adapter = spec.summary + spec.version.to_s
-      @machine = 'test' # remember to change this: The NetBIOS name of the local computer. This is set automatically by the .NET, PHP, and Java adapters
-      @ping_template = ERB.new(File.read("#{spec.gem_dir}/lib/spree_avatax_certified/templates/ping.erb"))
-      @adjust_tax_template = ERB.new(File.read("#{spec.gem_dir}/lib/spree_avatax_certified/templates/adjust_tax.erb"))
-      @client = Savon.client(wsdl: 'https://development.avalara.net/tax/taxsvcaltsec.wsdl', endpoint: URI.parse(Spree::Config.avatax_endpoint + "/Tax/TaxSvc.asmx"))
+      set_headers
+      @client = Savon.client(wsdl: 'https://development.avalara.net/tax/taxsvcaltsec.wsdl', endpoint: URI.parse(Spree::Config.avatax_endpoint + '/Tax/TaxSvc.asmx'))
     end
 
     def ping
+      @ping_template = ERB.new(File.read("#{spec.gem_dir}/lib/spree_avatax_certified/templates/ping.erb"))
       @soap = @ping_template.result(binding)
       @response = @client.call(:ping, xml: @soap).to_hash
     end
 
     def adjust_tax(request_hash)
       @AdjustmentReason = 4
-      @AdjustmentDescription = 'test' # remember to change this: Should be used when AdjustmentReason is “Other” for enhanced traceability. Leave empty for all AdjustmentReason selections except 8 - Other.
+      @AdjustmentDescription = 'Price or Quantity Adjusted'
+
+      @adjust_tax_template = ERB.new(File.read("#{spec.gem_dir}/lib/spree_avatax_certified/templates/adjust_tax.erb"))
 
       request_hash.each_pair do |k,v|
-        instance_variable_set("@#{k}",v)
+        instance_variable_set("@#{k}", v)
       end
 
       @soap = @adjust_tax_template.result(binding)
       @response = @client.call(:adjust_tax, xml: @soap).to_hash
+    end
+
+    private
+
+    def set_headers
+      @username = Spree::Config.avatax_account
+      @password = Spree::Config.avatax_license_key
+      @service_url = Spree::Config.avatax_endpoint
+      @clientname = AVATAX_CLIENT_VERSION
+      @spec = Gem::Specification.find_by_name('spree_avatax_certified')
+      @name = Rails.env
+      @adapter = spec.summary + spec.version.to_s
+      @machine = machine_name
+    end
+
+    def machine_name
+      platform = RUBY_PLATFORM.downcase
+      output = `#{(platform =~ /win32/) ? 'ipconfig /all' : 'ifconfig'}`
+      case platform
+      when /darwin/
+        $1 if output =~ /en1.*?(([A-F0-9]{2}:){5}[A-F0-9]{2})/im
+      when /win32/
+        $1 if output =~ /Physical Address.*?(([A-F0-9]{2}-){5}[A-F0-9]{2})/im
+      else
+        ''
+      end
     end
   end
 end
