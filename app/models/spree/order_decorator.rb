f524f0af5467eca78f286db42de27a6526fd287a
Allison C. Reilly <acreilly3@gmail.com>
2016-02-05 12:51:51 -0500

    removing rescues

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -19,33 +19,26 @@ Spree::Order.class_eval do
 
   def avalara_capture
     logger.debug 'avalara capture'
-    begin
-      create_avalara_transaction if avalara_transaction.nil?
-      line_items.reload
 
-      @rtn_tax = self.avalara_transaction.commit_avatax('SalesOrder')
+    create_avalara_transaction if avalara_transaction.nil?
+    line_items.reload
 
-      logger.info_and_debug('tax amount', @rtn_tax)
-      @rtn_tax
-    rescue => e
-      logger.debug e
-      logger.debug 'error in avalara capture'
-    end
+    @rtn_tax = self.avalara_transaction.commit_avatax('SalesOrder')
+
+    logger.info_and_debug('tax amount', @rtn_tax)
+    @rtn_tax
   end
 
   def avalara_capture_finalize
     logger.debug 'avalara capture finalize'
-    begin
-      create_avalara_transaction if avalara_transaction.nil?
-      line_items.reload
-      @rtn_tax = avalara_transaction.commit_avatax_final('SalesInvoice')
 
-      logger.info_and_debug('tax amount', @rtn_tax)
-      @rtn_tax
-    rescue => e
-      logger.debug e
-      logger.debug 'error in avalara capture finalize'
-    end
+    create_avalara_transaction if avalara_transaction.nil?
+    line_items.reload
+
+    @rtn_tax = avalara_transaction.commit_avatax_final('SalesInvoice')
+
+    logger.info_and_debug('tax amount', @rtn_tax)
+    @rtn_tax
   end
 
   def avatax_cache_key
