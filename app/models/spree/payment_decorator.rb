13456c9563da502930fd213994fd26a047a22dc0
Allison C. Reilly <acreilly3@gmail.com>
2016-02-05 17:11:29 -0500

    refactored avalara finalize

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -11,11 +11,12 @@ Spree::Payment.class_eval do
   end
 
   def avalara_finalize
+    return unless avalara_eligible?
+
     if self.amount != order.total
       self.update_attributes(amount: order.total)
-      order.avalara_capture_finalize if avalara_eligible?
-    else
-      order.avalara_capture_finalize if avalara_eligible?
     end
+
+    order.avalara_capture_finalize
   end
 end
