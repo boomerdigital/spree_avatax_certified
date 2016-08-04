895835627c047dab8d432b848303d507326c0eb6
Allison C. Reilly <acreilly3@gmail.com>
2016-08-04 16:17:03 -0400

    added config avatax vat id and added vat spec

--- app/controllers/spree/admin/avatax_settings_controller.rb
+++ app/controllers/spree/admin/avatax_settings_controller.rb
@@ -58,6 +58,7 @@ module Spree
         Spree::Config.avatax_endpoint = pref[:avatax_endpoint]
         Spree::Config.avatax_account = pref[:avatax_account]
         Spree::Config.avatax_license_key = pref[:avatax_license_key]
+        Spree::Config.avatax_vat_id = pref[:avatax_vat_id]
         Spree::Config.avatax_log = pref[:avatax_log]
         Spree::Config.avatax_address_validation = pref[:avatax_address_validation]
         Spree::Config.avatax_address_validation_enabled_countries = pref[:avatax_address_validation_enabled_countries]
