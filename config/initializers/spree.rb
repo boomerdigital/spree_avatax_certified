9a977b1caad3fc7322a9dd543b111f178a18cb3e
Allison C. Reilly <acreilly3@gmail.com>
2016-08-11 15:38:29 -0400

    Move adding AvalaraTransactionCalculator to to tax rates calculation to engine
    In spree.rb it was not appending the AvalaraTransactionCalculator to the tax_rates calculators array because tax_rates were nil on start up. Moving it to the engine, in an initalizer that is triggered after the calculators are set fixed the issue.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,3 +1,2 @@
 Spree.user_class = "Spree::User"
 Spree::PermittedAttributes.user_attributes.concat([:avalara_entity_use_code_id, :exemption_number, :vat_id])
-Rails.application.config.spree.calculators.tax_rates << Spree::Calculator::AvalaraTransactionCalculator
--- lib/spree_avatax_certified/engine.rb
+++ lib/spree_avatax_certified/engine.rb
@@ -27,6 +27,12 @@ module SpreeAvataxCertified
       end
     end
 
+    initializer 'spree.avatax_certified.calculators', after: 'spree.register.calculators' do |app|
+
+      Rails.application.config.spree.calculators.tax_rates << Spree::Calculator::AvalaraTransactionCalculator
+
+    end
+
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/models/**/*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
