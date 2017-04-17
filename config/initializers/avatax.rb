85cc53fb3e9a4ce0c5623435f4005c3194458e41
Allison C. Reilly <acreilly3@gmail.com>
2017-04-17 13:59:03 -0400

    Set up SPREE_BRANCH env

--- config/initializers/avatax.rb
+++ config/initializers/avatax.rb
@@ -1,5 +1,3 @@
-gem_version = File.read(File.expand_path('../../../GEM_VERSION',__FILE__)).strip
-
 AVATAX_CLIENT_VERSION = "a0o33000004FH8l"
 AVATAX_SERVICEPATH_ADDRESS = '/1.0/address/'
 AVATAX_SERVICEPATH_TAX = '/1.0/tax/'
