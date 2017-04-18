6ad24ff9db28d7cf3478ed160dd89767301183af
Allison C. Reilly <acreilly3@gmail.com>
2017-04-18 12:52:52 -0400

    Use quotes for params instead of symbols

--- app/controllers/spree/admin/avatax_settings_controller.rb
+++ app/controllers/spree/admin/avatax_settings_controller.rb
@@ -36,27 +36,27 @@ module Spree
         pref = params[:settings]
 
         Spree::Config.avatax_origin = {
-          Address1:  origin[:avatax_address1],
-          Address2: origin[:avatax_address2],
-          City: origin[:avatax_city],
-          Region: origin[:avatax_region],
-          Zip5: origin[:avatax_zip5],
-          Zip4: origin[:avatax_zip4],
-          Country: origin[:avatax_country]
+          Address1:  origin['avatax_address1'],
+          Address2: origin['avatax_address2'],
+          City: origin['avatax_city'],
+          Region: origin['avatax_region'],
+          Zip5: origin['avatax_zip5'],
+          Zip4: origin['avatax_zip4'],
+          Country: origin['avatax_country']
         }.to_json
 
-        Spree::Config.avatax_api_username = pref[:avatax_api_username]
-        Spree::Config.avatax_api_password = pref[:avatax_api_password]
-        Spree::Config.avatax_endpoint = pref[:avatax_endpoint]
-        Spree::Config.avatax_account = pref[:avatax_account]
-        Spree::Config.avatax_license_key = pref[:avatax_license_key]
-        Spree::Config.avatax_log = pref[:avatax_log]
-        Spree::Config.avatax_log = pref[:avatax_log_to_stdout]
-        Spree::Config.avatax_address_validation = pref[:avatax_address_validation]
-        Spree::Config.avatax_address_validation_enabled_countries = pref[:avatax_address_validation_enabled_countries]
-        Spree::Config.avatax_tax_calculation = pref[:avatax_tax_calculation]
-        Spree::Config.avatax_document_commit = pref[:avatax_document_commit]
-        Spree::Config.avatax_company_code = pref[:avatax_company_code]
+        Spree::Config.avatax_api_username = pref['avatax_api_username']
+        Spree::Config.avatax_api_password = pref['avatax_api_password']
+        Spree::Config.avatax_endpoint = pref['avatax_endpoint']
+        Spree::Config.avatax_account = pref['avatax_account']
+        Spree::Config.avatax_license_key = pref['avatax_license_key']
+        Spree::Config.avatax_log = pref['avatax_log'] || false
+        Spree::Config.avatax_log_to_stdout = pref['avatax_log_to_stdout'] || false
+        Spree::Config.avatax_address_validation = pref['avatax_address_validation'] || false
+        Spree::Config.avatax_tax_calculation = pref['avatax_tax_calculation'] || false
+        Spree::Config.avatax_document_commit = pref['avatax_document_commit'] || false
+        Spree::Config.avatax_address_validation_enabled_countries = pref['avatax_address_validation_enabled_countries']
+        Spree::Config.avatax_company_code = pref['avatax_company_code']
 
         respond_to do |format|
           format.html {
