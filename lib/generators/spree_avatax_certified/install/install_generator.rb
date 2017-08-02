c04051a956adaf7a03889baeac308d5a3f834394
Allison Reilly <acreilly3@gmail.com>
2017-08-02 18:40:40 -0400

    Add auto run migrations

--- lib/generators/spree_avatax_certified/install/install_generator.rb
+++ lib/generators/spree_avatax_certified/install/install_generator.rb
@@ -1,6 +1,7 @@
 module SpreeAvataxCertified
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
         append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_avatax_certified\n"
@@ -16,18 +17,9 @@ module SpreeAvataxCertified
         run 'bundle exec rake railties:install:migrations FROM=spree_avatax_certified'
       end
 
-      def auto_migrate?
-        ENV['AUTO_RUN_MIGRATIONS'] =~ /true/i
-      end
-
       def run_migrations
-        # hiding this inside parent method so it's not auto-run by rails generator
-        def migration_prompt_approved?
-          result = ask('Would you like to run the migrations now? [Y/n]')
-          !(result =~ /n/i)
-        end
-
-        if auto_migrate? || migration_prompt_approved?
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migrations
           run 'bundle exec rake db:migrate'
         else
           puts 'Skipping rake db:migrate, don\'t forget to run it!'
