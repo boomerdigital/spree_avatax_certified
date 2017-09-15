ff833583e42bc4f4dc01f8b6e98f96f0fb65e944
Oskar Janusz <janusz.oskar@gmail.com>
2017-09-15 15:21:43 +0200

    fix mismatch_error

--- app/models/spree_avatax_certified/request/get_tax.rb
+++ app/models/spree_avatax_certified/request/get_tax.rb
@@ -1,27 +1,23 @@
-module SpreeAvataxCertified
-  module Request
-    class GetTax < SpreeAvataxCertified::Request::Base
-      def generate
-        @request = {
-          DocCode: order.number,
-          DocDate: doc_date,
-          Discount: order.all_adjustments.promotion.eligible.sum(:amount).abs.to_s,
-          Commit: @commit,
-          DocType: @doc_type ? @doc_type : 'SalesOrder',
-          Addresses: address_lines,
-          Lines: sales_lines
-        }.merge(base_tax_hash)
+class SpreeAvataxCertified::Request::GetTax < SpreeAvataxCertified::Request::Base
+  def generate
+    @request = {
+      DocCode: order.number,
+      DocDate: doc_date,
+      Discount: order.all_adjustments.promotion.eligible.sum(:amount).abs.to_s,
+      Commit: @commit,
+      DocType: @doc_type ? @doc_type : 'SalesOrder',
+      Addresses: address_lines,
+      Lines: sales_lines
+    }.merge(base_tax_hash)
 
-        check_vat_id
+    check_vat_id
 
-        @request
-      end
+    @request
+  end
 
-      protected
+  protected
 
-      def doc_date
-        order.completed? ? order.completed_at.strftime('%F') : Date.today.strftime('%F')
-      end
-    end
+  def doc_date
+    order.completed? ? order.completed_at.strftime('%F') : Date.today.strftime('%F')
   end
 end
--- app/models/spree_avatax_certified/response/get_tax.rb
+++ app/models/spree_avatax_certified/response/get_tax.rb
@@ -1,11 +1,7 @@
-module SpreeAvataxCertified
-  module Response
-    class GetTax < SpreeAvataxCertified::Response::Base
-      alias :tax_result :result
+class SpreeAvataxCertified::Response::GetTax < SpreeAvataxCertified::Response::Base
+  alias :tax_result :result
 
-      def description
-        'Get Tax'
-      end
-    end
+  def description
+    'Get Tax'
   end
 end
