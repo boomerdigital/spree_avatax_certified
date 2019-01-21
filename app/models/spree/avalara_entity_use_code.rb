23d6d98e4bbc6ff554358ec0a29a69b2474364c6
Allison Reilly <acreilly3@gmail.com>
2019-01-22 02:57:07 +0530

    Use Spree::Base

--- app/models/spree/avalara_entity_use_code.rb
+++ app/models/spree/avalara_entity_use_code.rb
@@ -1,5 +1,5 @@
 module Spree
-  class AvalaraEntityUseCode < ActiveRecord::Base
+  class AvalaraEntityUseCode < Spree::Base
     has_many :users, class_name: Spree.user_class.to_s, foreign_key: :avalara_entity_use_code_id
   end
 end
