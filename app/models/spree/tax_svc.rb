269684e9fafcc54b9d840b72d9f228433efb5523
Allison Reilly <acreilly3@gmail.com>
2019-01-22 03:15:29 +0530

    Update VCR calls

--- app/models/spree/tax_svc.rb
+++ app/models/spree/tax_svc.rb
@@ -119,8 +119,8 @@ class TaxSvc
 
   def request(uri, request_hash)
     res = RestClient::Request.execute(method: :post,
-                                timeout: 1,
-                                open_timeout: 1,
+                                open_timeout: 2,
+                                read_timeout: 6,
                                 url: service_url + uri,
                                 payload:  JSON.generate(request_hash),
                                 headers: {
